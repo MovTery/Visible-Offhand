81f8b47df971a23437781f6ead381e3b0bb41423
MovTery <123115192+HopiHopy@users.noreply.github.com>
2024-01-21 08:12:39 +0800

    1.2

--- src/main/java/com/movtery/visible_offhand/mixin/Config_SkinOptionsScreenMixin.java
+++ src/main/java/com/movtery/visible_offhand/mixin/Config_SkinOptionsScreenMixin.java
@@ -7,13 +7,14 @@ import net.minecraft.client.gui.screen.ScreenTexts;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.SkinOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.CyclingButtonWidget;
+import net.minecraft.client.gui.widget.OptionButtonWidget;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.Option;
 import net.minecraft.client.render.entity.PlayerModelPart;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,6 +25,9 @@ import static com.movtery.visible_offhand.client.VisibleOffhandClient.getConfig;
 @Environment(EnvType.CLIENT)
 @Mixin(SkinOptionsScreen.class)
 public abstract class Config_SkinOptionsScreenMixin extends GameOptionsScreen {
+    @Shadow
+    protected abstract Text getPlayerModelPartDisplayString(PlayerModelPart part);
+
     public Config_SkinOptionsScreenMixin(Screen parent, GameOptions gameOptions, Text title) {
         super(parent, gameOptions, title);
     }
@@ -36,24 +40,32 @@ public abstract class Config_SkinOptionsScreenMixin extends GameOptionsScreen {
         PlayerModelPart[] var2 = PlayerModelPart.values();
 
         for (PlayerModelPart playerModelPart : var2) {
-            this.addDrawableChild(CyclingButtonWidget.onOffBuilder(this.gameOptions.isPlayerModelPartEnabled(playerModelPart)).build(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, playerModelPart.getOptionName(), (button, enabled) -> this.gameOptions.togglePlayerModelPart(playerModelPart, enabled)));
+            this.addButton(new ButtonWidget(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, this.getPlayerModelPartDisplayString(playerModelPart), (button) -> {
+                this.gameOptions.togglePlayerModelPart(playerModelPart);
+                button.setMessage(this.getPlayerModelPartDisplayString(playerModelPart));
+            }));
             ++i;
         }
 
-        this.addDrawableChild(Option.MAIN_HAND.createButton(this.gameOptions, this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150));
+        this.addButton(new OptionButtonWidget(this.width / 2 - 155 + i % 2 * 160, this.height / 6 + 24 * (i >> 1), 150, 20, Option.MAIN_HAND, Option.MAIN_HAND.getMessage(this.gameOptions), (button) -> {
+            Option.MAIN_HAND.cycle(this.gameOptions, 1);
+            this.gameOptions.write();
+            button.setMessage(Option.MAIN_HAND.getMessage(this.gameOptions));
+            this.gameOptions.sendClientSettings();
+        }));
         ++i;
         if (i % 2 == 1) {
             ++i;
         }
 
         //创建一个新的按钮，用于控制开关双手显示
-        this.addDrawableChild(CyclingButtonWidget.onOffBuilder(onOrOff(getConfig().getOptions().doubleHands), onOrOff(!getConfig().getOptions().doubleHands))
-                .build(this.width / 2 - 102, this.height / 6 + 24 * (i >> 1), 100, 20, new TranslatableText("button.vo.double_hands"), (button, enabled) -> {
+        this.addButton(new ButtonWidget(this.width / 2 - 102, this.height / 6 + 24 * (i >> 1), 100, 20, onOrOff(getConfig().getOptions().doubleHands), (button) -> {
                     getConfig().getOptions().doubleHands = !getConfig().getOptions().doubleHands;
                     getConfig().save();
+                    button.setMessage(onOrOff(getConfig().getOptions().doubleHands));
                 }));
 
-        this.addDrawableChild(new ButtonWidget(this.width / 2 + 2, this.height / 6 + 24 * (i >> 1), 100, 20, ScreenTexts.DONE, (button) -> {
+        this.addButton(new ButtonWidget(this.width / 2 + 2, this.height / 6 + 24 * (i >> 1), 100, 20, ScreenTexts.DONE, (button) -> {
             if (this.client != null) {
                 this.client.openScreen(this.parent);
             }
@@ -64,9 +76,9 @@ public abstract class Config_SkinOptionsScreenMixin extends GameOptionsScreen {
     private Text onOrOff(Boolean button) {
         TranslatableText translatableText;
         if (button) {
-            translatableText = new TranslatableText("button.vo.on");
+            translatableText = (TranslatableText) new TranslatableText("button.vo.double_hands").append(":").append(new TranslatableText("button.vo.on"));
         } else {
-            translatableText = new TranslatableText("button.vo.off");
+            translatableText = (TranslatableText) new TranslatableText("button.vo.double_hands").append(":").append(new TranslatableText("button.vo.off"));
         }
         return translatableText;
     }
