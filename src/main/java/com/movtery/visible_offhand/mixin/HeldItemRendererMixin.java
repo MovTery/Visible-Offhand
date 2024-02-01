cef990ca67f2de6ec1193b35b7a20491a9c75189
MovTery <123115192+HopiHopy@users.noreply.github.com>
2024-02-01 12:58:36 +0800

    1.2.1

--- src/main/java/com/movtery/visible_offhand/mixin/HeldItemRendererMixin.java
+++ src/main/java/com/movtery/visible_offhand/mixin/HeldItemRendererMixin.java
@@ -6,9 +6,11 @@ import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,8 +29,10 @@ public abstract class HeldItemRendererMixin {
     private void doubleHands(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
         if (getConfig().getOptions().doubleHands) {
             boolean mainHand = hand == Hand.MAIN_HAND;
+            Item mainHandItem = player.getMainHandStack().getItem();
+            String mainHandItemId = Registry.ITEM.getId(mainHandItem).toString();
             Arm offArm = mainHand ? player.getMainArm() : player.getMainArm().getOpposite();
-            if (item.isEmpty() && (!mainHand && !player.isInvisible())) {
+            if (item.isEmpty() && !getConfig().getOptions().handheldItems.contains(mainHandItemId) && (!mainHand && !player.isInvisible())) {
                 this.renderArmHoldingItem(matrices, vertexConsumers, light, equipProgress, swingProgress, offArm);
             }
         }
